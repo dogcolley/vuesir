1de07a8cff2653c3d0697498661f06af63816683
dogcolley <jinji0326@naver.com>
2021-03-18 18:24:37 +0900

    모달 상단까지 진행완료

---
+++ components/common/TopInfo.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="top-info">
+        <v-row>
+            <v-col cols="2">
+                <div class="profile">
+                    <img src="" alt="">
+                </div>
+            </v-col>
+
+            <v-col cols="10">
+                <v-row class="top-con">
+                    <v-col v-for="(info, id) in info_arr" :key="id" :md="cols[id < col_length ? id :  id - col_length  * Math.floor(id / col_length)].md" :cols="cols[id < col_length ? id :  id - col_length  * Math.floor(id / col_length)].cols" >
+                        <div>
+                            <strong v-html="info.tit"></strong>
+                            <span v-html="info.txt"></span>
+                        </div>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        img:String,
+        info_arr:Array,
+        cols:Array,
+    },
+    data(){
+        return{
+            col_length:0
+        }
+    },
+    created(){
+        this.col_length = this.cols.length
+    }
+}
+</script>
+
+<style lang="scss">
+
+
+    .top-info{
+        .profile{ background:pink;height:100%}
+        .top-con{
+            /* > * {padding-right:0;padding-top:0} */
+        }
+
+    }
+</style>
--- components/modal/MemberInfo.vue
+++ components/modal/MemberInfo.vue
@@ -12,35 +12,35 @@
             </v-btn>
         </header>
         <article class="con">
-
-
-            <div>
-                
-                <img src="" alt="">
-                
-                <ul>
-                    <li></li>
-                </ul>
-
-                <ul>
-                    <li></li>
-                </ul>
-                
-                <ul>
-                    <li></li>
-                </ul>
-                
-            </div>
+            <TopInfo :info_arr="top_info" :img="top_img" :cols="top_col"/>
 
             <div>
-                <ul>
-                    <li></li>
-                </ul>
+                <v-tabs
+                    class="tab1"
+                    v-model="tab"
+                    background-color="primary"
+                    dark
+                >
+                    <v-tab
+                    v-for="item in items"
+                    :key="item.tab"
+                    >
+                    {{ item.tab }}
+                    </v-tab>
+                </v-tabs>
+            
+                <v-tabs-items v-model="tab">
+                    <v-tab-item
+                    v-for="item in items"
+                    :key="item.tab"
+                    >
+                    <v-card flat>
+                        <v-card-text>{{ item.content }}</v-card-text>
+                    </v-card>
+                    </v-tab-item>
+                </v-tabs-items>
+                
 
-
-                <ul>
-                    <li></li>
-                </ul>
             </div>
 
         </article>
@@ -48,10 +48,58 @@
 </template>
 
 <script>
+import TopInfo from '~/components/common/TopInfo.vue';
+
 export default {
     props:{
         type : String
+    },
+    components:{
+        TopInfo
+    },
+    data(){
+        return{
+            top_col:[
+                {cols:"12",md:"3"},
+                {cols:"12",md:"5"},
+                {cols:"12",md:"2"},
+                {cols:"12",md:"2"},
+            ],
+            top_info:[
+                {tit:'이름'       , txt:'이름 (성별) 나이'}, 
+                {tit:'블로그'     , txt:"https://domain.com/testlink"}, 
+                {tit:'신청'       , txt:'186'}, 
+                {tit:'상태 '      , txt:'우수'}, 
+                {tit:'닉네임'     , txt:'닉네임예시입니다'}, 
+                {tit:'인스타그램' , txt:'https://domain.com/testlink'}, 
+                {tit:'선정'       , txt:'96'}, 
+                {tit:'포인트'     , txt:'180,500'}, 
+                {tit:'연락처'     , txt:'010-1111-2222'}, 
+                {tit:'유투브'     , txt:'https://domain.com/testlink'}, 
+                {tit:'리뷰'       , txt:'50'}, 
+                {tit:'가입일'     , txt:'21-01-02'}, 
+                {tit:'활동지역'   , txt:'시도/구군'}, 
+                {tit:'카페'       , txt:'https://domain.com/testlink'}, 
+                {tit:'지연/취소'  , txt:'3%/1%'}, 
+                {tit:'최근접속'   , txt:'21.01-03'}, 
+            ],
+            top_img:'',
+            tab: null,
+            items: [
+                { tab: 'One'     , content: 'Tab 1 Content' },
+                { tab: 'Two'     , content: 'Tab 2 Content' },
+                { tab: 'Three'   , content: 'Tab 3 Content' },
+                { tab: 'Four'    , content: 'Tab 4 Content' },
+                { tab: 'Five'    , content: 'Tab 5 Content' },
+                { tab: 'Six'     , content: 'Tab 6 Content' },
+                { tab: 'Seven'   , content: 'Tab 7 Content' },
+                { tab: 'Eight'   , content: 'Tab 8 Content' },
+                { tab: 'Nine'    , content: 'Tab 9 Content' },
+                { tab: 'Ten'     , content: 'Tab 10 Content' },
+            ],
+        }
     }
+
 }
 </script>
 
@@ -63,6 +111,14 @@ export default {
         }
         .con{height: calc( 100% - 51px );padding:20px;box-sizing: border-box;overflow: auto;}
 
-
+        .tab1{}
+        .v-item-group{background:none!important;
+            * {color: #000 !important}
+        }
+        .v-slide-group__wrapper{border-left:1px solid #d9d9d9;margin-top:20px}
+        .v-tabs-slider{background:#d9d9d9!important}
+        .v-tab{border:1px solid #d9d9d9;border-left:0;background:#f9f9f9;border-bottom:0}
+        .v-tab--active{background:#fff;border-bottom:0}
+        .v-tabs-items{border-top:1px solid #d9d9d9}
     }
 </style>
